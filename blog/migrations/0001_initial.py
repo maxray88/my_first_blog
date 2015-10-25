2842c1c3f692c69232c4350c7694d05c01682c5d
maxray88 <maoyidong@gmail.com>
2015-10-25 23:48:47 +0100

    My blog app, first commit

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('created_date', models.DateTimeField(default=datetime.datetime(2015, 10, 23, 22, 14, 24, 109972, tzinfo=utc))),
+                ('published_date', models.DateTimeField(null=True, blank=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
